759b49324dddfc3b834f6f8f4abcee81848f215a
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2024-09-11 00:58:54 +0530

    Backend up and running for AUTH

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const AuthRouter = require('./Routes/AuthRouter');
+
+require('dotenv').config();
+require('./Models/db');
+
+
+const PORT = process.env.PORT || 8080
+
+app.get('/ping', (req, res) => {
+    res.send('PONG');
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/auth', AuthRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
