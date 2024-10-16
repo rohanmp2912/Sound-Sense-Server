5857cde86c71b0e1925872cc8838159e2be3cc02
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2024-10-17 00:34:21 +0530

    ADD: Route to add score

--- Routes/PlayerRouter.js
+++ Routes/PlayerRouter.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const { getXY, getTreasureArray, addTreasureBox } = require('../Controllers/PlayerController')
+const { getXY, getTreasureArray, addTreasureBox, addScore } = require('../Controllers/PlayerController')
 
 router.get('/getXY', getXY);
 router.get('/getTreasureArray', getTreasureArray);
-router.post('/addTreasureBox', addTreasureBox)
+router.post('/addTreasureBox', addTreasureBox);
+router.post('/addScore', addScore);
 
 module.exports = router;
