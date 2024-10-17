4f4321c584ec138bc02930448c8f1c5ea4b38442
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2024-10-17 12:02:09 +0530

    ADD: Route to update Coordinates

--- Routes/PlayerRouter.js
+++ Routes/PlayerRouter.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const { getXY, getTreasureArray, addTreasureBox, addScore } = require('../Controllers/PlayerController')
+const { getXY, getTreasureArray, addTreasureBox, addScore, updateCoordinates } = require('../Controllers/PlayerController')
 
 router.get('/getXY', getXY);
 router.get('/getTreasureArray', getTreasureArray);
 router.post('/addTreasureBox', addTreasureBox);
 router.post('/addScore', addScore);
+router.post('/updateCoordinates', updateCoordinates)
 
 module.exports = router;
